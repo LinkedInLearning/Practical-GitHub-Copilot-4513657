7ddc51ed7079a342e02d1130d3d52ddd43c80764
DESKTOP-S959DPI\macni <mac.nishimura@gmail.com>
2023-11-09 18:12:20 +0900

    add end files

---
+++ src_end/ReadFile.py
@@ -0,0 +1,10 @@
+# Copilotによって自動生成されたコードです。必ずしも同じコードが生成されるとは限りません。
+def ReadFileaAndPrintName():
+    # Read the file
+    file = open("sample.txt", "r")
+
+    # Print the name of the file
+    print(file.name)
+
+    # Close the file
+    file.close()
